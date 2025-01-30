ec597769ede9d3ad3e911dc95bf98221234bf4f5
Justin Finn <122637243+Justinfinn2891@users.noreply.github.com>
2025-01-30 14:03:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,28 @@ document.getElementById("yes-button").onclick = function()
 {
     console.log("work");
     document.getElementById("face").src = "look2.webp";
+    document.getElementById("yes-button").style.display = "none";
+    document.getElementById("no-button").style.display = "none";
+    document.getElementById("heart").style.display = "none";
+}
+
+document.getElementById("yes").onclick = function()
+{
+    console.log("work");
+    document.getElementById("style-class").style.display = "none";
+    document.getElementById("no-button").style.display = "none";
+}
+
+document.getElementById("no").onclick = function()
+{
+    console.log("work");
+    document.getElementById("no").style.display = "none";
+    
+}
+
+document.getElementById("no-button").onclick = function()
+{
+  
+    document.getElementById("box-wrapp").style.display = "flex";
+    
 }
